fc63cd2884412e2d9962bf14ea412e2c6802c178
ViolentDelites <71895865+ViolentDelites@users.noreply.github.com>
2024-03-25 11:50:30 -0400

    Add files via upload

--- LookupCode.cs
+++ LookupCode.cs
@@ -3,9 +3,20 @@ namespace ISB.CLWater.Data.Entities
     [Table("LookupCode", Schema = "dbo")]
     public partial class LookupCode
     {
-        public int Id { get; set; }
-        public string CodeType { get; set; }
-        public string Name { get; set; }
+        [StringLength(50)]
+        public int Code { get; set; }
+        [StringLength(100)]
+        public string? Name { get; set; }
+        [StringLength(2000)]
+        public string? Description { get; set; }
+        [StringLength(10)]
+        public string? Abbreviation { get; set; }
+        public bool? IsActive { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? UpdatedBy { get; set;}
+        public DateTime? UpdatedDate { get; set; }
+
         // ... other relevant properties if needed
     }
 }
