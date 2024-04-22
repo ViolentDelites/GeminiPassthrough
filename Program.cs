951ccf633c11ecfe35c78a89a88862190863e3cd
ViolentDelites <71895865+ViolentDelites@users.noreply.github.com>
2024-04-22 16:22:04 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,11 +2,27 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 var connectionString = configuration.GetConnectionString("SQLConnection");
 
-builder.Services.AddDbContext<CLWaterContext>(options => options.UseSqlServer(connectionString));
+// DbContext and DBContextFactory registrations
+builder.Services.AddDbContextPool<CLWaterContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContextFactory<CLWaterContext>(options => options.UseSqlServer(connectionString));
+
+// Razor Pages and Blazor registrations
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
+// Other service registrations
 ServiceDependencyInjection.RegisterServices(builder.Services);
 
+// Set the default layout
+builder.Services.AddScoped(sp =>
+{
+    var navigationManager = sp.GetRequiredService<NavigationManager>();
+    return new HttpClient { BaseAddress = new Uri(navigationManager.BaseUri) };
+});
+builder.Services.AddSingleton<MainLayout>();
+
+// Build the application
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,17 +32,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
-// Set the default layout
-builder.Services.AddScoped(sp =>
-{
-    var navigationManager = sp.GetRequiredService<NavigationManager>();
-    return new HttpClient { BaseAddress = new Uri(navigationManager.BaseUri) };
-});
-builder.Services.AddSingleton<MainLayout>();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
