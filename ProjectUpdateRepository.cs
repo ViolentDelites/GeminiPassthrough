de16bd429bf6283c8140fabd9a6754fcead2f67a
ViolentDelites <71895865+ViolentDelites@users.noreply.github.com>
2024-04-22 15:00:44 -0400

    Add files via upload

--- ProjectUpdateRepository.cs
+++ ProjectUpdateRepository.cs
@@ -1,13 +1,30 @@
-namespace ISB.CLWater.Service.Repositories
+namespace ISB.CLWater.Service.Repositories;
+
+public interface IProjectUpdateRepository : ICLWaterRepository<ProjectUpdate>
 {
-    public interface IProjectUpdateRepository : ICLWaterRepository<ProjectUpdate>
+    Task InsertProjectUpdateAsync(ProjectUpdate projectUpdate);
+    Task<List<ProjectUpdate>> GetTop3ProjectUpdates();
+}
+public class ProjectUpdateRepository : CLWaterRepository<ProjectUpdate>, IProjectUpdateRepository
+{
+    private readonly IDbContextFactory<CLWaterContext> _contextFactory;
+    public ProjectUpdateRepository(IDbContextFactory<CLWaterContext> contextFactory)
+        : base(contextFactory)
     {
+        _contextFactory = contextFactory;
     }
-    public class ProjectUpdateRepository : CLWaterRepository<ProjectUpdate>, IProjectUpdateRepository
+
+    public async Task InsertProjectUpdateAsync(ProjectUpdate projectUpdate)
     {
-        public ProjectUpdateRepository(IDbContextFactory<CLWaterContext> contextFactory)
-            : base(contextFactory)
+        Add(projectUpdate);
+        await SaveChangesAsync();
+    }
+
+    public async Task<List<ProjectUpdate>> GetTop3ProjectUpdates()
+    {
+        using (var context = _contextFactory.CreateDbContext())
         {
+            return await context.ProjectUpdate.OrderByDescending(p => p.ArticleDate).Take(3).ToListAsync();
         }
     }
 }
