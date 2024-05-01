0f7865ac572f6f6fdae402f0bce84ffaf5d8da5b
ViolentDelites <71895865+ViolentDelites@users.noreply.github.com>
2024-05-01 14:14:30 -0400

    Add files via upload

--- InquiryRepository.cs
+++ InquiryRepository.cs
@@ -2,6 +2,7 @@ namespace ISB.CLWater.Service.Repositories
 {
     public interface IInquiryRepository : ICLWaterRepository<Inquiry>
     {
+        Task<IEnumerable<InquiryDTO>> GetInquiriesAsync(DateTime startDate, DateTime endDate, int? inquiryTypeId = null);
     }
     public class InquiryRepository : CLWaterRepository<Inquiry>, IInquiryRepository
     {
@@ -15,6 +16,31 @@ namespace ISB.CLWater.Service.Repositories
             _contextFactory = contextFactory;
             _userRepository = userRepository;
             _lookupCodeRepository = lookupCodeRepository;
+        }
+
+        public async Task<IEnumerable<InquiryDTO>> GetInquiriesAsync(DateTime startDate, DateTime endDate, int? inquiryTypeId = null)
+        {
+            using (var context = _contextFactory.CreateDbContext())
+            {
+                return await context.TBL_INQUIRY
+                        .Where(inq => inq.CREATED_DATE >= startDate && inq.CREATED_DATE <= endDate)
+                        .Where(inq => inquiryTypeId == null || inq.INQUIRY_TYPE_ID == inquiryTypeId)
+                        .Select(inq => new InquiryDTO
+                        {
+                            InquiryId = inq.INQUIRY_ID,
+                            InquiryTypeId = inq.INQUIRY_TYPE_ID,
+                            InquiryTypeDescription = _lookupCodeRepository.GetLookupCodeDescription(inq.INQUIRY_TYPE_ID),
+                            Comments = inq.COMMENTS,
+                            Notes = inq.NOTES,
+                            CreatedDate = inq.CREATED_DATE,
+                            CreatedBy = inq.CREATED_BY,
+                            CreatedByName = _userRepository.GetUserFullName(inq.CREATED_BY),
+                            EditedDate = inq.EDITED_DATE,
+                            EditedBy = inq.EDITED_BY,
+                        })
+                        .OrderByDescending(inq => inq.InquiryId)
+                        .ToListAsync();
+            }
         }
     }
 }
