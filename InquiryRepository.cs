f81420b182c05833d7a206b7d1a743d42d17d6c5
ViolentDelites <71895865+ViolentDelites@users.noreply.github.com>
2024-05-01 11:46:14 -0400

    Add files via upload

--- InquiryRepository.cs
+++ InquiryRepository.cs
@@ -6,10 +6,15 @@ namespace ISB.CLWater.Service.Repositories
     public class InquiryRepository : CLWaterRepository<Inquiry>, IInquiryRepository
     {
         private readonly IDbContextFactory<CLWaterContext> _contextFactory;
-        public InquiryRepository(IDbContextFactory<CLWaterContext> contextFactory)
+        private readonly IUserRepository _userRepository;
+        private readonly ILookupCodeRepository _lookupCodeRepository;
+
+        public InquiryRepository(IDbContextFactory<CLWaterContext> contextFactory, IUserRepository userRepository, ILookupCodeRepository lookupCodeRepository)
             : base(contextFactory)
         {
             _contextFactory = contextFactory;
+            _userRepository = userRepository;
+            _lookupCodeRepository = lookupCodeRepository;
         }
     }
 }
